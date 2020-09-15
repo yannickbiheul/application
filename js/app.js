13a20ae5b11fa6c08e8e131baf2694f80701b795
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-15 22:56:18 +0200

    1ère tentative d'animation

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let titreApp = document.getElementById("titreApp");
+let cadreApp = document.querySelector(".cadreApp");
+let vitesse = 2;
+let hauteurTitre = parseFloat(getComputedStyle(titreApp).height);
+let animationId = null;
+console.log(getComputedStyle(titreApp).top);
+console.log(getComputedStyle(cadreApp).height);
+console.log(hauteurTitre);
+
+// Déplacer le titre vers le bas jusqu'au bord du cadre
+function deplacerTitreBas() {
+    let yTitre = parseFloat(getComputedStyle(titreApp).top);
+    let yMax = parseFloat(getComputedStyle(cadreApp).height) / 2;
+    if (yTitre + hauteurTitre <= yMax) {
+        titreApp.style.top = (yTitre + vitesse) + "px";
+        animationId = requestAnimationFrame(deplacerTitreBas);
+    } else {
+        titreApp.style.letterSpacing = "6px";
+        // cancelAnimationFrame(deplacerTitreBas);
+        requestAnimationFrame(deplacerTitreHaut);
+    }
+}
+
+function deplacerTitreHaut() {
+    let yTitre = parseFloat(getComputedStyle(titreApp).bottom);
+    let yMax = parseFloat(getComputedStyle(cadreApp).height) / 2;
+    if (yTitre + hauteurTitre <= yMax) {
+        titreApp.style.bottom = (yTitre + vitesse) + "px";
+        animationId = requestAnimationFrame(deplacerTitreHaut);
+    } else {
+        cancelAnimationFrame(deplacerTitreHaut);
+        requestAnimationFrame(deplacerTitreBas);
+    }
+}
+
+animationId = requestAnimationFrame(deplacerTitreBas);
