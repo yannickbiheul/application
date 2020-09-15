5607bde8546345a5702cac4b2ffee8238e93ff48
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-15 23:30:12 +0200

    Amélioration rebours

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let compteurElt = document.getElementById("titreApp");
+let timeoutId;
 
 function diminuerCompteur() {
     let compteur = Number(compteurElt.textContent);
@@ -6,10 +7,19 @@ function diminuerCompteur() {
         compteurElt.textContent = compteur - 1;
     } else {
         clearInterval();
-        compteurElt.textContent = "APPLICATIONS";
-        compteurElt.style.fontSize = "4em";
+        compteurElt.textContent = "Bienvenue dans";
+        timeoutId = window.setTimeout(afficherTitre, 2000);
+        clearAlert();
     }
     
+}
+
+function afficherTitre() {
+    compteurElt.textContent = "APPLICATIONS";
+}
+
+function clearAlert() {
+    window.clearTimeout(timeoutId);
 }
 
 setInterval(diminuerCompteur, 1000);
